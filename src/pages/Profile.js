55561126e83210727cc0b71a653ae7d7dc7b20dc
yash.patel1 <yash.patel@drcsystems.com>
2023-01-20 20:35:05 +0530

    a tag added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getProfile } from "../services/profileServices";
 
 function Profile() {
   const { id } = useParams();
   const [data, setData] = useState({});
   const [error, setError] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getProfile(id)
@@ -14,7 +15,14 @@ function Profile() {
   }, [id]);
   return (
     <>
-      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
         <Link className="back-btn" to={"/profiles"}>
           Go Back
         </Link>
@@ -42,17 +50,29 @@ function Profile() {
             <h2>{data.gender}</h2>
             <h2>{data.location?.name}</h2>
             <h2 className={data.status}>{data.status}</h2>
-            <p>Click Here!</p>
+            {/* <p> */}{" "}
+            <a
+              // style={{ color: "white", textDecoration: "none" }}
+              href={data.url}
+              target="_blank"
+            >
+              Click Here!
+            </a>
+            {/* </p> */}
           </div>
           <div className="particular-profile-card-right">
             <h1>Episode List</h1>
             {data?.episode?.map((item) => (
-              <a href={item} target="_blank">
+              <a className="episode-link" href={item} target="_blank">
                 {" "}
                 {item}
               </a>
             ))}
-            <p>Click Here!</p>
+            {/* <p> */}
+            <a className="a-link" href={data.url} target="_blank">
+              Click Here!
+            </a>
+            {/* </p>   */}
           </div>
         </div>
       )}
